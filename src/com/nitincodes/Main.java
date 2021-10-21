01a2ada78149e5f4406497fdd8e50ecc4f8c75b1
Nitin <nsinghboss02@gmail.com>
2021-10-21 21:46:47 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,24 @@
+package com.nitincodes;
+
+public class Main {
+// predict the output:
+    public static void main(String[] args) {
+	// write your code here
+        Abc.method();
+    }
+}
+class Abc{
+    void method(){
+        System.out.println("method is calling:");
+    }
+}
+
+
+
+
+/* it will show an error because of two reasons
+1. we have called the method with the name of the class, it means the method should be static but here the method is not
+static so we can not call it by the name of the class
+2. if the method is not static then we can go for creation of an object and a constructor too but here we haven't
+done anything something like that
+ */
